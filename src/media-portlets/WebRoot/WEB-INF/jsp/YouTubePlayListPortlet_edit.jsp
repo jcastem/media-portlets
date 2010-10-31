e89ae13e9f82a942624f92f3f4549a170ca057cc
Juan Carlos Castellanos Moreno <jcastem@dev-portal.com>
2010-10-31 15:35:47 -0500

    YouTubePlaylist: Adicion de busqueda de videos por usuario y por tags

--- src/media-portlets/WebRoot/WEB-INF/jsp/YouTubePlayListPortlet_edit.jsp
+++ src/media-portlets/WebRoot/WEB-INF/jsp/YouTubePlayListPortlet_edit.jsp
@@ -8,6 +8,13 @@
 <%--<%PortletPreferences prefs = renderRequest.getPreferences();%>--%> 
 
 <form action="${requestScope.savePlaylistURL}" method="POST">
+	Tipo de lista de reproducci&oacute;n:
+	<select name="playlist_type" onchange="mostrarVideoId(this.value, 'div_video_id')"> 
+		<option value="playlist" ${requestScope.playlistType == "playlist"? 'selected="selected"': '' }>Playlist</option>
+		<option value="search" ${requestScope.playlistType == "search"? 'selected="selected"': '' }>Busqueda</option>
+	</select>
 	C&oacute;digo del playlist <input name = "playlist" value="${requestScope.playlist}" type="text" />
+	Usuario: <input name = "user" value="${requestScope.user}" type="text" />
+	Etiquetas: <input name = "tags" value="${requestScope.tags}" type="text" />
 	<input type="submit" value="Guardar">  
 </form>
--- src/media-portlets/src/com/devportal/portlet/media/controller/YouTubePlayListPortlet.java
+++ src/media-portlets/src/com/devportal/portlet/media/controller/YouTubePlayListPortlet.java
@@ -21,13 +21,20 @@ import javax.portlet.RenderResponse;
 import javax.portlet.ValidatorException;
 
 import com.devportal.portlet.media.util.YouTubeUtil;
+import com.google.gdata.client.Query;
+import com.google.gdata.client.youtube.YouTubeQuery;
 import com.google.gdata.client.youtube.YouTubeService;
+import com.google.gdata.data.Category;
 import com.google.gdata.data.youtube.PlaylistLinkEntry;
 import com.google.gdata.data.youtube.PlaylistLinkFeed;
+import com.google.gdata.data.youtube.YouTubeNamespace;
 import com.google.gdata.util.ServiceException;
 
 public class YouTubePlayListPortlet extends GenericPortlet {
 
+	private static final String PLAYLIST_TYPE_PLAYLIST = "playlist";
+	private static final String PLAYLIST_TYPE_SEARCH = "search"; 
+	
 	/**
 	 * Helper method to serve up the mandatory view mode.
 	 */
@@ -47,8 +54,14 @@ public class YouTubePlayListPortlet extends GenericPortlet {
 			throws PortletException, IOException {
 		// Cargamos la configuracion del playlist del portlet
 		PortletPreferences prefs = request.getPreferences();
+		String playlistType = prefs.getValue("playlistType", null);
 		String playlist = prefs.getValue("playlist", "");
+		String user = prefs.getValue("user", "");
+		String tags = prefs.getValue("tags", "");
+		request.setAttribute("playlistType", playlistType);
 		request.setAttribute("playlist", playlist);
+		request.setAttribute("user", user);
+		request.setAttribute("tags", tags);
 		
 		response.setContentType("text/html");
 		PortletRequestDispatcher dispatcher = getPortletContext().getRequestDispatcher("/WEB-INF/jsp/YouTubePlayListPortlet_edit.jsp");
@@ -68,21 +81,51 @@ public class YouTubePlayListPortlet extends GenericPortlet {
 		// Cargamos el codigo del playlist de las preferencias del portlet
 		PortletPreferences prefs = request.getPreferences();
 		String playlist = prefs.getValue("playlist", null);
+		String playlistType = prefs.getValue("playlistType", null);
+		String user = prefs.getValue("user", null);
+		String tags = prefs.getValue("tags", null);
 		
 		YouTubeService service = YouTubeUtil.getYouTubeServiceInstance();
 		int maxResults = 10; 
 		int start = 1; // Indice de comienzo del feed por defecto
 		Integer paginaActual = 1; // Indice de pagina por defecto
 		int totalResults = 0;
-		PlaylistLinkFeed videoFeed;
+		PlaylistLinkFeed videoFeed = null;
 		int totalPaginas = 0;
-		if(playlist != null && !playlist.trim().isEmpty()) {
+		String wsUrl = "http://gdata.youtube.com/feeds/api/playlists";
+		Query.CategoryFilter categoryFilter;
+		YouTubeQuery query = null;
+			
+		if(playlistType != null && !playlistType.trim().isEmpty()) {
+			
+			if(playlistType.equals(YouTubePlayListPortlet.PLAYLIST_TYPE_SEARCH)) {
+				query = new YouTubeQuery(new URL("http://gdata.youtube.com/feeds/api/videos"));
+				categoryFilter = new Query.CategoryFilter();
+				if(tags != null && !tags.trim().isEmpty()) {
+					String [] arrTags = tags.trim().split(" ");
+					for(String tag: arrTags) {	
+						categoryFilter.addCategory(new Category(YouTubeNamespace.KEYWORD_SCHEME, tag.trim()));
+					}
+					query.addCategoryFilter(categoryFilter);
+				}
+				
+				//wsUrl = "http://gdata.youtube.com/feeds/api/videos";
+		        
+		        if(user != null && !user.trim().isEmpty()) {
+		        	query.setAuthor(user);
+		        }
+			}
 			
 			// Invocamos un primer feed para obtener el tamano de la lista de videos
-			String feedUrl = "http://gdata.youtube.com/feeds/api/playlists/" + playlist + "?orderby=published&start-index=" + start + "&max-results=" + maxResults;
+			String feedUrl = wsUrl + "/" + playlist + "?orderby=published&start-index=" + start + "&max-results=" + maxResults;
 			
 			try {
-				videoFeed = service.getFeed(new URL(feedUrl), PlaylistLinkFeed.class);
+				if(playlistType.equals(YouTubePlayListPortlet.PLAYLIST_TYPE_PLAYLIST)) {
+					videoFeed = service.getFeed(new URL(feedUrl), PlaylistLinkFeed.class);
+				}
+				else if(playlistType.equals(YouTubePlayListPortlet.PLAYLIST_TYPE_SEARCH)) {
+					 videoFeed = service.query(query, PlaylistLinkFeed.class);
+				}
 				totalResults = videoFeed.getTotalResults();
 				// Calculo del total de paginas
 				if(totalResults % maxResults == 0) {
@@ -137,12 +180,20 @@ public class YouTubePlayListPortlet extends GenericPortlet {
 			*/
 						
 		
-			feedUrl = "http://gdata.youtube.com/feeds/api/playlists/" + playlist + "?orderby=published&start-index=" + start + "&max-results=" + maxResults;
+			feedUrl = wsUrl + "/" + playlist + "?orderby=published&start-index=" + start + "&max-results=" + maxResults;
 			System.out.println("FEED LIST: " + feedUrl);
 			List<Map<String,String>> feeds = new ArrayList<Map<String,String>>();
 			try {
 				//VideoFeed videoFeed = service.getFeed(new URL(feedUrl), VideoFeed.class);
-				videoFeed = service.getFeed(new URL(feedUrl), PlaylistLinkFeed.class);
+				if(playlistType.equals(YouTubePlayListPortlet.PLAYLIST_TYPE_PLAYLIST)) {
+					videoFeed = service.getFeed(new URL(feedUrl), PlaylistLinkFeed.class);
+				}
+				else if(playlistType.equals(YouTubePlayListPortlet.PLAYLIST_TYPE_SEARCH)) {
+					query.setMaxResults(maxResults);
+					query.setStartIndex(start);
+					videoFeed = service.query(query, PlaylistLinkFeed.class);
+					System.out.println("RES: " + totalResults);
+				}
 				for(PlaylistLinkEntry entry : videoFeed.getEntries() ) {
 					Map<String, String> feed = new HashMap<String,String>();
 					feed.put("title", entry.getTitle().getPlainText());
@@ -177,23 +228,34 @@ public class YouTubePlayListPortlet extends GenericPortlet {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}		
+			response.setContentType("text/html");
+			PortletRequestDispatcher dispatcher = getPortletContext()
+			.getRequestDispatcher(
+					"/WEB-INF/jsp/YouTubePlayListPortlet_view.jsp");
+			dispatcher.include(request, response);
 		}
-		
-		response.setContentType("text/html");
-		PortletRequestDispatcher dispatcher = getPortletContext()
-		.getRequestDispatcher(
-				"/WEB-INF/jsp/YouTubePlayListPortlet_view.jsp");
-		dispatcher.include(request, response);
 		
 	}
 	
 	@ProcessAction(name="savePlaylist") 
 	public void editAction(ActionRequest actionRequest, ActionResponse actionResponse) {
+		String playlistType = actionRequest.getParameter("playlist_type");
 		String playlist = actionRequest.getParameter("playlist");
+		String user = actionRequest.getParameter("user");
+		String tags = actionRequest.getParameter("tags");
 		PortletPreferences prefs = actionRequest.getPreferences();
-		if(playlist != null & !playlist.trim().isEmpty()) {
+		if(playlistType != null & !playlistType.trim().isEmpty()) {
 			try {
-				prefs.setValue("playlist", playlist);
+				prefs.setValue("playlistType", playlistType);
+				if(playlist != null & !playlist.trim().isEmpty()) {
+					prefs.setValue("playlist", playlist.trim());
+				}
+				if(user != null & !user.trim().isEmpty()) {
+					prefs.setValue("user", user.trim());
+				}
+				if(tags != null & !tags.trim().isEmpty()) {
+					prefs.setValue("tags", tags.trim());
+				}
 				prefs.store();
 			} catch (ReadOnlyException e) {
 				// TODO Auto-generated catch block
